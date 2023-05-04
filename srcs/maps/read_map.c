ff459013748b22711057d3835e7622ab370498c8
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-05-04 22:02:51 +0900

    add: 生成されたオブジェクトファイルをobjs/ で生成されるようにした

--- srcs/maps/read_map.c
+++ srcs/maps/read_map.c
@@ -6,42 +6,39 @@
 /*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 16:34:10 by susasaki          #+#    #+#             */
-/*   Updated: 2023/04/28 18:55:06 by susasaki         ###   ########.fr       */
+/*   Updated: 2023/05/04 21:37:02 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../include/cub3d.h"
+#include "../include/cub3d.h"
 
-static void filename_check(char *filename)
+static void	filename_check(char *filename)
 {
-    char *cub;
-    int     i;
-    int     len;
+	char	*cub;
+	int		i;
+	int		len;
 
-    len = ft_strlen(filename);
-    cub = ".cub";
-    i = ft_strlen(cub);
-    while (cub[i--] == filename[len--])
-    {
-        if (i == 0)
-            break;
-    }
-    if (cub[i] != filename[len])
-        print_error("filename");
-    return ;
-    
+	len = ft_strlen(filename);
+	cub = ".cub";
+	i = ft_strlen(cub);
+	while (cub[i--] == filename[len--])
+	{
+		if (i == 0)
+			break ;
+	}
+	if (cub[i] != filename[len])
+		print_error("filename");
+	return ;
 }
 
-
-
-void read_map(char *path,t_info *info)
+void	read_map(char *path, t_info *info)
 {
-    int fd;
-    // char *str;
+	int	fd;
 
-    filename_check(path);
-    fd = open(path, O_RDONLY);
-    if (fd == -1)
-        print_error("open");
-    get_map_data(fd,info);
+	// char *str;
+	filename_check(path);
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		print_error("open");
+	get_map_data(fd, info);
 }
