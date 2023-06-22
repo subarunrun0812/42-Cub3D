b64f0e6c6bce39fb68374d5a43e7040bbde5c19d
Hidenobuk <>
2023-06-22 18:58:05 +0900

    床と天井を描画する処理のサンプル作成

--- hnoguchi/lodev/sample/raycaster_floor.c
+++ hnoguchi/lodev/sample/raycaster_floor.c
@@ -6,7 +6,7 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 10:14:40 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/06/22 10:49:09 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/06/22 15:56:17 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ int world_map[MAP_WIDTH][MAP_HEIGHT] =
 		float	x_ray_direction_1 = vars->x_direction + vars->x_camera_plane; // float	rayDirX1 = dirX + planeX;
 		float	y_ray_direction_1 = vars->y_direction + vars->y_camera_plane; // float	rayDirY1 = dirY + planeY;
 		// Current y position compared to the center of the screen (the horizon)
-		int		y_corrent_position = y - (vars->screen_height / 2); // int		p = y - screenHeight / 2;
+		int		y_current_position = y - (vars->screen_height / 2); // int		p = y - screenHeight / 2;
 
 		// Vertical position of the camera.
 		// NOTE:
@@ -76,47 +76,72 @@ int world_map[MAP_WIDTH][MAP_HEIGHT] =
 		// The ratio was 1 / p for going through the camera plane,
 		// so to go posZ times farther to reach the floor,
 		// we get that the total horizontal distance is posZ / p.
-		float rowDistance = posZ / p;
+		float	row_distance = vertical_position_camera / y_current_position; // float rowDistance = posZ / p;
 
 		// calculate the real world step vector we have to add for each x (parallel to camera plane)
 		// adding step by step avoids multiplications with a weight in the inner loop
-		float floorStepX = rowDistance * (rayDirX1 - rayDirX0) / screenWidth;
-		float floorStepY = rowDistance * (rayDirY1 - rayDirY0) / screenWidth;
+		float	x_step_floor = row_distance * (x_ray_direction_1 - x_ray_direction_0) / vars->screen_width; // float floorStepX = rowDistance * (rayDirX1 - rayDirX0) / screenWidth;
+		float	y_step_floor = row_distance * (y_ray_direction_1 - y_ray_direction_0) / vars->screen_width; // float floorStepY = rowDistance * (rayDirY1 - rayDirY0) / screenWidth;
 		
 		// real world coordinates of the leftmost column. This will be updated as we step to the right.
-		float floorX = posX + rowDistance * rayDirX0;
-		float floorY = posY + rowDistance * rayDirY0;
-		
-		for(int x = 0; x < screenWidth; ++x)
+		float	x_floor = vars->x_position_vector + row_distance * x_ray_direction_0; // float floorX = posX + rowDistance * rayDirX0;
+		float	y_floor = vars->y_position_vector + row_distance * y_ray_direction_0; // float floorY = posY + rowDistance * rayDirY0;
+	
+		int	x;
+
+		x = 0;
+		while (x < vars->screen_width)
 		{
+			x += 1;
 			// the cell coord is simply got from the integer parts of floorX and floorY
-			int cellX = (int)(floorX);
-			int cellY = (int)(floorY);
+			int	x_cell = (int)x_floor; // int cellX = (int)(floorX);
+			int y_cell = (int)y_floor; // int cellY = (int)(floorY);
 			
 			// get the texture coordinate from the fractional part
-			int tx = (int)(texWidth * (floorX - cellX)) & (texWidth - 1);
-			int ty = (int)(texHeight * (floorY - cellY)) & (texHeight - 1);
+			// TODO: Change name texWidth
+			int x_texture_coordinate = (int)(texWidth * (x_floor - x_cell)) & (texWidth - 1); // int tx = (int)(texWidth * (floorX - cellX)) & (texWidth - 1);
+			// TODO: Change name texHeight
+			int y_texture_coordinate = (int)(texHeight * (y_floor - y_cell)) & (texHeight - 1); // int ty = (int)(texHeight * (floorY - cellY)) & (texHeight - 1);
 			
-			floorX += floorStepX;
-			floorY += floorStepY;
+			x_floor += x_step_floor;
+			y_floor += y_step_floor;
 			
 			// choose texture and draw the pixel
-			int checkerBoardPattern = (int(cellX + cellY)) & 1;
-			int floorTexture;
-			if(checkerBoardPattern == 0) floorTexture = 3;
-			else floorTexture = 4;
-			int ceilingTexture = 6;
-			Uint32 color;
+			int	checker_board_pattern = (int)((x_cell + y_cell) & 1); // int	checkerBoardPattern = (int(cellX + cellY)) & 1;
+			int	floor_texture; // int	floorTexture;
+
+			if (checker_board_pattern == 0)
+			{
+				floor_texture = 3;
+			}
+			else
+			{
+				floor_texture = 4;
+			}
+			int	ceiling_texture = 6; // int	ceilingTexture = 6;
+
+			// Uint32 color;
+			// 
+			// // floor
+			// color = texture[floorTexture][texWidth * ty + tx];
+			// color = (color >> 1) & 8355711; // make a bit darker
+			// buffer[y][x] = color;
+			// 
+			// //ceiling (symmetrical, at screenHeight - y - 1 instead of y)
+			// color = texture[ceilingTexture][texWidth * ty + tx];
+			// color = (color >> 1) & 8355711; // make a bit darker
+			// buffer[screenHeight - y - 1][x] = color;
+			unsigned int	color;
 			
 			// floor
-			color = texture[floorTexture][texWidth * ty + tx];
-			color = (color >> 1) & 8355711; // make a bit darker
-			buffer[y][x] = color;
-			
-			//ceiling (symmetrical, at screenHeight - y - 1 instead of y)
-			color = texture[ceilingTexture][texWidth * ty + tx];
-			color = (color >> 1) & 8355711; // make a bit darker
-			buffer[screenHeight - y - 1][x] = color;
+			color = *(vars->texture_list[floor_texture].data.addr + vars->texture_list[floor_texture].height * texture_y + texture_x);
+			color = (color >> 1) & 8355711;
+			vars->image.addr[y * vars->screen_width + x_axis] = color;
+
+			// ceiling
+			color = *(vars->texture_list[ceiling_texture].data.addr + vars->texture_list[ceiling_texture].height * texture_y + texture_x);
+			color = (color >> 1) & 8355711;
+			vars->image.addr[y * vars->screen_width + x_axis] = color;
 		}
 	}
 #endif // FLOOR_HORIZONTAL
