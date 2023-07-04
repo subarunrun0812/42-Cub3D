23ef4db3b08c427928c0091be06858e9cc7abdde
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-07-04 16:47:56 +0900

    add:床天井がtextureの場合、rgbの各値を-1ずつにした

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 18:50:28 by susasaki          #+#    #+#             */
-/*   Updated: 2023/07/04 16:25:17 by susasaki         ###   ########.fr       */
+/*   Updated: 2023/07/04 16:34:24 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	init_second(t_info *info, t_flag *flag, t_texture *texture)
 {
 	info->flag = flag;
 	info->texture = texture;
-	info->flag->map = 1;
+	info->flag->map = -1;
 }
 
 void	init(t_info *info, t_map *map, t_vars *vars, t_data *data)
