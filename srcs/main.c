f72e6352e27fa6477956e41b331e3667458fbdb2
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-07-05 13:59:31 +0900

    fix: 構造体rgbに値が入っていない問題を解決した。

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 12:58:01 by susasaki          #+#    #+#             */
-/*   Updated: 2023/07/04 16:01:28 by susasaki         ###   ########.fr       */
+/*   Updated: 2023/07/05 13:58:47 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,11 +38,15 @@ int	main(int argc, char **argv)
 	t_data data;
 	t_flag flag;
 	t_texture texture;
-	
+	t_rgb   f_rgb;
+    t_rgb   c_rgb;
+
 	if (argc != 2)
 		print_error("no map specified.");
 	init(&info, &map, &vars, &data);
 	init_second(&info, &flag, &texture);
+	info.texture->f_rgb = &f_rgb; 
+	info.texture->c_rgb = &c_rgb; 
 	read_file(argv[1], &info);
 	init_player_coordinate(&map,&info);
 	raycasting(&info);
