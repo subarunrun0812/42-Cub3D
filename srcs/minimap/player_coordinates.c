d5d33efed0b3a22a54f03a3d02fba5366dda0bb7
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-07-06 16:48:59 +0900

    norm:複数のファイルをnorm対応

--- srcs/minimap/player_coordinates.c
+++ srcs/minimap/player_coordinates.c
@@ -6,32 +6,33 @@
 /*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 16:44:42 by susasaki          #+#    #+#             */
-/*   Updated: 2023/07/06 16:02:38 by susasaki         ###   ########.fr       */
+/*   Updated: 2023/07/06 16:46:51 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../../include/cub3d.h"
+#include "../../include/cub3d.h"
 
-void init_player_coordinate(t_map *map,t_info *info)
+void	init_player_coordinate(t_map *map, t_info *info)
 {
-    int _y;
-    int _x;
-    _y = 0;
-    while (map->map_data[_y])
-    {
-        _x = 0;
-        while (map->map_data[_y][_x])
-        {
-            if (map->map_data[_y][_x] == 'N' || map->map_data[_y][_x] == 'S'
-            || map->map_data[_y][_x] == 'W' || map->map_data[_y][_x] == 'E')
-            {
+	int	_y;
+	int	_x;
+
+	_y = 0;
+	while (map->map_data[_y])
+	{
+		_x = 0;
+		while (map->map_data[_y][_x])
+		{
+			if (map->map_data[_y][_x] == 'N' || map->map_data[_y][_x] == 'S'
+				|| map->map_data[_y][_x] == 'W' || map->map_data[_y][_x] == 'E')
+			{
 				info->map->y_player = _y;
-                info->map->x_player = _x;
-                return ;
-            }
-            _x++;
-        }
-        _y++;
-    }
-    return ;
+				info->map->x_player = _x;
+				return ;
+			}
+			_x++;
+		}
+		_y++;
+	}
+	return ;
 }
--- srcs/minimap/updata_pos_map.c
+++ srcs/minimap/updata_pos_map.c
@@ -6,7 +6,7 @@
 /*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 15:03:20 by susasaki          #+#    #+#             */
-/*   Updated: 2023/07/06 16:16:12 by susasaki         ###   ########.fr       */
+/*   Updated: 2023/07/06 16:46:44 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,20 @@
 
 void	updata_pos_map(t_vars *vars, t_info *info, int keycode)
 {
-	// map情報を更新
 	if (info->map->map_data[(int)(vars->x_pos)][(int)(vars->y_pos)] == '0')
 	{
 		if (keycode == W_KEY)
 		{
-			info->map->map_data[(int)(vars->x_pos
-				- (vars->x_dir
-					* MOVE_DIST))][(int)(vars->y_pos
-				- (vars->y_dir * MOVE_DIST))] = '0';
+			info->map->map_data
+			[(int)(vars->x_pos - (vars->x_dir * MOVE_DIST))]
+			[(int)(vars->y_pos - (vars->y_dir * MOVE_DIST))] = '0';
 			info->map->map_data[(int)(vars->x_pos)][(int)(vars->y_pos)] = 'N';
 		}
 		else if (keycode == S_KEY)
 		{
-			info->map->map_data[(int)(vars->x_pos
-				+ (vars->x_dir
-					* MOVE_DIST))][(int)(vars->y_pos
-				+ (vars->y_dir * MOVE_DIST))] = '0';
+			info->map->map_data
+			[(int)(vars->x_pos + (vars->x_dir * MOVE_DIST))]
+			[(int)(vars->y_pos + (vars->y_dir * MOVE_DIST))] = '0';
 			info->map->map_data[(int)(vars->x_pos)][(int)(vars->y_pos)] = 'N';
 		}
 	}
