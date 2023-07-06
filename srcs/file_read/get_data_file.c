5ff9a04c872dc177bb45f96244ead7225ac52c45
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-07-06 17:13:57 +0900

    delete:条件式の項目の2,3,4の文字があった場合を削除した

--- srcs/file_read/get_data_file.c
+++ srcs/file_read/get_data_file.c
@@ -6,7 +6,7 @@
 /*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 18:01:49 by susasaki          #+#    #+#             */
-/*   Updated: 2023/07/05 13:58:49 by susasaki         ###   ########.fr       */
+/*   Updated: 2023/07/06 17:13:02 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,8 @@ static int	non_specific_chara(char *str)
 	i = 0;
 	while (str[i] != '\n' && str[i] != '\0')
 	{
-		// TODO: 課題要件にない'2','3','4'を削除する
 		if (str[i] != '0' && str[i] != '1' && str[i] != 'N' && str[i] != 'S'
-			&& str[i] != 'E' && str[i] != 'W' && str[i] != ' ' && str[i] != '2'
-			&& str[i] != '3' && str[i] != '4')
+			&& str[i] != 'E' && str[i] != 'W' && str[i] != ' ')
 			return (1);
 		i++;
 	}
@@ -80,16 +78,16 @@ static char	**map_str_add(char **array, char *new)
 	return (res);
 }
 
-int check_texture_section(char *str)
+int	check_texture_section(char *str)
 {
-    int i;
-    i = 0;
+	int	i;
 
-    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\0')
-        i++;
-    if ('0' <= str[i] && str[i] <= '9')
-        return (-1);
-    return (0);
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\0')
+		i++;
+	if ('0' <= str[i] && str[i] <= '9')
+		return (-1);
+	return (0);
 }
 
 void	get_data_file(int fd, t_info *info)
@@ -98,6 +96,7 @@ void	get_data_file(int fd, t_info *info)
 	int		flag;
 	int		count;
 	int		i;
+
 	str = get_next_line(fd);
 	if (!str)
 	{
@@ -105,21 +104,20 @@ void	get_data_file(int fd, t_info *info)
 	}
 	flag = 0;
 	i = 0;
-    count = 0;
+	count = 0;
 	while (str)
 	{
 		if (check_texture_section(str) == 0)
 		{
-            count += read_texture(str,info->texture);
+			count += read_texture(str, info->texture);
 			free(str);
-            str = get_next_line(fd);
+			str = get_next_line(fd);
 			i++;
-            // printf("count = %d\n",count);
 		}
-        else if (count < 6)
+		else if (count < 6)
 		{
 			free_texture(info);
-            print_error("texture num");
+			print_error("texture num");
 		}
 		else
 		{
@@ -139,18 +137,19 @@ void	get_data_file(int fd, t_info *info)
 			}
 		}
 	}
-    printf("\x1b[32m");
-    printf("texture->no=%s\n",info->texture->no);
-    printf("texture->so=%s\n",info->texture->so);
-    printf("texture->we=%s\n",info->texture->we);
-    printf("texture->ea=%s\n",info->texture->ea);
-    printf("texture->f_tex=%s\n",info->texture->f_tex);
-    printf("texture->c_tex=%s\n",info->texture->c_tex);
-    printf("texture->floor->red=%d\n",info->texture->f_rgb->red);
-    printf("texture->floor->green=%d\n",info->texture->f_rgb->green);
-    printf("texture->floor->blue=%d\n",info->texture->f_rgb->blue);
-    printf("texture->celling->red=%d\n",info->texture->c_rgb->red);
-    printf("texture->celling->green=%d\n",info->texture->c_rgb->green);
-    printf("texture->celling->blue=%d\n",info->texture->c_rgb->blue);
-    printf("\x1b[0m\n");		
 }
+
+// printf("\x1b[32m");
+// printf("texture->no=%s\n",info->texture->no);
+// printf("texture->so=%s\n",info->texture->so);
+// printf("texture->we=%s\n",info->texture->we);
+// printf("texture->ea=%s\n",info->texture->ea);
+// printf("texture->f_tex=%s\n",info->texture->f_tex);
+// printf("texture->c_tex=%s\n",info->texture->c_tex);
+// printf("texture->floor->red=%d\n",info->texture->f_rgb->red);
+// printf("texture->floor->green=%d\n",info->texture->f_rgb->green);
+// printf("texture->floor->blue=%d\n",info->texture->f_rgb->blue);
+// printf("texture->celling->red=%d\n",info->texture->c_rgb->red);
+// printf("texture->celling->green=%d\n",info->texture->c_rgb->green);
+// printf("texture->celling->blue=%d\n",info->texture->c_rgb->blue);
+// printf("\x1b[0m\n");
