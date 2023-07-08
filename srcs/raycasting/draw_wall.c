a2dafe2f1df0ca77607ab6492029a1d3250e725b
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2023-07-08 11:56:49 +0900

    comment: draw_wall

--- srcs/raycasting/draw_wall.c
+++ srcs/raycasting/draw_wall.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   draw_wall.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: susasaki <susasaki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 14:33:56 by hnoguchi          #+#    #+#             */
-/*   Updated: 2023/07/07 16:28:20 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/07/08 11:56:29 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+// valueの絶対値を返す関数
 double	abs_double(double value)
 {
 	if (value == 0.0)
@@ -55,29 +56,41 @@ static void	put_texture(t_draw_texture *texture, t_draw_wall *wall,
 
 static void	set_ray_data(t_ray *ray, t_vars *vars, int x)
 {
+	//画面上のpixel値(x座標)を視野の範囲に交換します
 	double	x_current_cam;
 
 	x_current_cam = 2 * x / (double)WIN_WIDTH - 1;
+	//レイの方向 = カメラの向き + カメラ平面に対する現在の位置(x_current_cam)
 	ray->x_dir = vars->x_dir + (vars->x_cam_plane
 			* x_current_cam);
 	ray->y_dir = vars->y_dir + (vars->y_cam_plane
 			* x_current_cam);
+	// プレイヤーの位置をマップ上の整数座標として設定する
 	ray->x_map = (int)vars->x_pos;
 	ray->y_map = (int)vars->y_pos;
+	//初期化
 	ray->x_side_dist = 0;
 	ray->y_side_dist = 0;
+	//もしレイがX方向に全く進まない(水平)な場合、x_delta_distは無限大として扱われるため、
+	//それに近い1e30を設定する
 	if (ray->x_dir == 0)
 	{
 		ray->x_delta_dist = 1e30;
 	}
 	else
+	{
+		// 水平に1グリッド進むのに必要な[実距離]を表す
 		ray->x_delta_dist = abs_double(1 / ray->x_dir);
+	}
 	if (ray->y_dir == 0)
 	{
 		ray->y_delta_dist = 1e30;
 	}
 	else
+	{
+		// 垂直に1グリッド進むのに必要な[実距離]を表す
 		ray->y_delta_dist = abs_double(1 / ray->y_dir);
+	}
 }
 
 int	draw_wall(t_info *info)
