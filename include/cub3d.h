d4eb0a15ec56cede973ec3e3f44d70087fd0924d
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2023-07-08 10:48:17 +0900

    fix: mergeのときに削除されたコメントを復元

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: susasaki <susasaki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 19:50:22 by susasaki          #+#    #+#             */
-/*   Updated: 2023/07/07 16:29:53 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/07/08 10:47:53 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -173,14 +173,19 @@ typedef struct s_vars
 {
 	void				*mlx;
 	void				*win;
+	// playerのx,y position
 	double				x_pos;
 	double				y_pos;
+	// playerが向いている向き
 	double				x_dir;
 	double				y_dir;
+	//カメラ平面のx,y成分(FOV)。-1から1の範囲
 	double				x_cam_plane;
 	double				y_cam_plane;
 	unsigned int		floor_col;
 	unsigned int		ceil_col;
+	t_data				*data;
+	t_texture_data		texture_list[TEXTURE_LIST_SIZE];
 }						t_vars;
 
 typedef struct s_flag
@@ -190,12 +195,17 @@ typedef struct s_flag
 
 typedef struct s_ray
 {
+	// rayベクトルのx,y成分
 	double				x_dir;
 	double				y_dir;
+	// rayのマップ上の現在のブロックのx,y座標
 	int					x_map;
 	int					y_map;
+	// rayが座標上の整数値に当たるまでのx,y距離
 	double				x_side_dist;
 	double				y_side_dist;
+	// rayが次のx,y方向のブロックの境界に到達するたびに
+	// side_distanceに加えるべき距離を表します。
 	double				x_delta_dist;
 	double				y_delta_dist;
 }						t_ray;
@@ -209,30 +219,43 @@ struct					s_info
 	t_texture			texture;
 	t_texture_data		texture_list[TEXTURE_LIST_SIZE];
 };
-
 typedef struct s_draw_wall
 {
+	// rayが衝突した軸がx軸かy軸か判定する。
 	int					side;
+	// playerの現在位置からそのrayが衝突した壁までの距離。perpendicular=垂直
 	double				wall_dist;
+	// 描画する壁の高さ
 	int					line_height;
+	// 壁の描画を開始する画面のy座標
 	int					start_y;
+	// 壁の描画を終了する画面のy座標
 	int					end_y;
 }						t_draw_wall;
 
 typedef struct s_draw_texture
 {
+	// 描画する壁のテクスチャ
 	int					index;
+	// 描画する壁（テクスチャ）のx軸上の位置
 	double				wall_x;
+	// 描画する壁のx座標
 	int					x_coord;
+	// 描画する壁の間隔
 	double				span;
+	// 現在描画する壁の位置
 	double				current_pos;
 }						t_draw_texture;
 
 typedef struct s_draw_background
 {
+	// 描画するテクスチャの列の間隔(x軸)
 	float				x_span;
+	// 描画するテクスチャの行の間隔(y軸)
 	float				y_span;
+	// 描画するテクスチャのx座標
 	float				x_coord;
+	// 描画するテクスチャのy座標
 	float				y_coord;
 }						t_draw_background;
 
