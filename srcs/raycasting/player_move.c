1b6aa9b6d384a26072ebd841dfa6b4a37f71ac08
subarunrun0812 <subarunrun0812@yahoo.co.jp>
2023-07-11 21:13:31 +0900

    refactor: プレイヤーの移動処理

--- srcs/raycasting/player_move.c
+++ srcs/raycasting/player_move.c
@@ -6,7 +6,7 @@
 /*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 19:54:54 by susasaki          #+#    #+#             */
-/*   Updated: 2023/07/11 21:03:16 by susasaki         ###   ########.fr       */
+/*   Updated: 2023/07/11 21:12:24 by susasaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,15 @@ void	move_backward(char **map, t_vars *vars)
 	int		one_backward_y_pos_vec;
 	char	distination;
 
-	one_backward_x_pos_vec = vars->x_pos - (vars->x_dir * MOVE_DIST);
-	one_backward_y_pos_vec = vars->y_pos - (vars->y_dir * MOVE_DIST);
+	one_backward_x_pos_vec = vars->x_pos + (-vars->x_dir * MOVE_DIST);
+	one_backward_y_pos_vec = vars->y_pos + (-vars->y_dir * MOVE_DIST);
 	distination = map[one_backward_x_pos_vec][one_backward_y_pos_vec];
 	if (distination == '1')
 		return ;
 	else
 	{
-		vars->x_pos -= vars->x_dir * MOVE_DIST;
-		vars->y_pos -= vars->y_dir * MOVE_DIST;
+		vars->x_pos += -vars->x_dir * MOVE_DIST;
+		vars->y_pos += -vars->y_dir * MOVE_DIST;
 	}
 }
 
@@ -55,14 +55,14 @@ void	move_right(char **map, t_vars *vars)
 	char	destination;
 
 	one_right_x_pos_vec = vars->x_pos + (vars->y_dir * MOVE_DIST);
-	one_right_y_pos_vec = vars->y_pos - (vars->x_dir * MOVE_DIST);
+	one_right_y_pos_vec = vars->y_pos + (-vars->x_dir * MOVE_DIST);
 	destination = map[one_right_x_pos_vec][one_right_y_pos_vec];
 	if (destination == '1')
 		return ;
 	else
 	{
 		vars->x_pos += vars->y_dir * MOVE_DIST;
-		vars->y_pos -= vars->x_dir * MOVE_DIST;
+		vars->y_pos += -vars->x_dir * MOVE_DIST;
 	}
 }
 
@@ -72,7 +72,7 @@ void	move_left(char **map, t_vars *vars)
 	int one_left_y_pos_vec;
 	char destination;
 
-	one_left_x_pos_vec = vars->x_pos - (vars->y_dir * MOVE_DIST);
+	one_left_x_pos_vec = vars->x_pos + (-vars->y_dir * MOVE_DIST);
 	one_left_y_pos_vec = vars->y_pos + (vars->x_dir * MOVE_DIST);
 	destination = map[one_left_x_pos_vec][one_left_y_pos_vec];
 
@@ -80,7 +80,7 @@ void	move_left(char **map, t_vars *vars)
 		return ;
 	else
 	{
-		vars->x_pos -= vars->y_dir * MOVE_DIST;
+		vars->x_pos += -vars->y_dir * MOVE_DIST;
 		vars->y_pos += vars->x_dir * MOVE_DIST;
 	}
 }
