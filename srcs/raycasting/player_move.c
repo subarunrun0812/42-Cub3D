3bef9896b17be2b1c473996b5245bf56e1ace25b
Hidenobuk <>
2023-07-13 19:02:51 +0900

    壁のすり抜けを解消

--- srcs/raycasting/player_move.c
+++ srcs/raycasting/player_move.c
@@ -6,11 +6,13 @@
 /*   By: susasaki <susasaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 19:54:54 by susasaki          #+#    #+#             */
-/*   Updated: 2023/07/12 14:09:43 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2023/07/13 19:01:30 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+# define PLAYER_BODY 0.4
 
 void	move_forward(char **map, t_vars *vars)
 {
@@ -20,14 +22,12 @@ void	move_forward(char **map, t_vars *vars)
 
 	one_forward_x_pos_vec = vars->x_pos + (vars->x_dir * MOVE_DIST);
 	one_forward_y_pos_vec = vars->y_pos + (vars->y_dir * MOVE_DIST);
-	distination = map[one_forward_x_pos_vec][one_forward_y_pos_vec];
-	if (distination == '1')
-		return ;
-	else
-	{
+	distination = map[one_forward_x_pos_vec][(int)vars->y_pos];
+	if (distination != '1')
 		vars->x_pos += vars->x_dir * MOVE_DIST;
+	distination = map[(int)vars->x_pos][one_forward_y_pos_vec];
+	if (distination != '1')
 		vars->y_pos += vars->y_dir * MOVE_DIST;
-	}
 }
 
 void	move_backward(char **map, t_vars *vars)
@@ -38,14 +38,12 @@ void	move_backward(char **map, t_vars *vars)
 
 	one_backward_x_pos_vec = vars->x_pos + (-vars->x_dir * MOVE_DIST);
 	one_backward_y_pos_vec = vars->y_pos + (-vars->y_dir * MOVE_DIST);
-	distination = map[one_backward_x_pos_vec][one_backward_y_pos_vec];
-	if (distination == '1')
-		return ;
-	else
-	{
+	distination = map[one_backward_x_pos_vec][(int)vars->y_pos];
+	if (distination != '1')
 		vars->x_pos += -vars->x_dir * MOVE_DIST;
+	distination = map[(int)vars->x_pos][one_backward_y_pos_vec];
+	if (distination != '1')
 		vars->y_pos += -vars->y_dir * MOVE_DIST;
-	}
 }
 
 void	move_right(char **map, t_vars *vars)
@@ -56,14 +54,12 @@ void	move_right(char **map, t_vars *vars)
 
 	one_right_x_pos_vec = vars->x_pos + (vars->y_dir * MOVE_DIST);
 	one_right_y_pos_vec = vars->y_pos + (-vars->x_dir * MOVE_DIST);
-	destination = map[one_right_x_pos_vec][one_right_y_pos_vec];
-	if (destination == '1')
-		return ;
-	else
-	{
+	destination = map[one_right_x_pos_vec][(int)vars->y_pos];
+	if (destination != '1')
 		vars->x_pos += vars->y_dir * MOVE_DIST;
+	destination = map[(int)vars->x_pos][one_right_y_pos_vec];
+	if (destination != '1')
 		vars->y_pos += -vars->x_dir * MOVE_DIST;
-	}
 }
 
 void	move_left(char **map, t_vars *vars)
@@ -74,12 +70,10 @@ void	move_left(char **map, t_vars *vars)
 
 	one_left_x_pos_vec = vars->x_pos + (-vars->y_dir * MOVE_DIST);
 	one_left_y_pos_vec = vars->y_pos + (vars->x_dir * MOVE_DIST);
-	destination = map[one_left_x_pos_vec][one_left_y_pos_vec];
-	if (destination == '1')
-		return ;
-	else
-	{
+	destination = map[one_left_x_pos_vec][(int)vars->y_pos];
+	if (destination != '1')
 		vars->x_pos += -vars->y_dir * MOVE_DIST;
+	destination = map[(int)vars->x_pos][one_left_y_pos_vec];
+	if (destination != '1')
 		vars->y_pos += vars->x_dir * MOVE_DIST;
-	}
 }
